5058ed94810116da7a5cc472ac77639cccda30bc
SMei201900 <smei201900@gmail.com>
2021-07-18 00:21:17 -0400

    Made changes to HTML and JS File
    Added <div class> to allow Bootstrap to work.
    Added a bunch of functions to the JS File. Allow C-F degree conversion, Search and Current Location button to work.

--- script.js
+++ script.js
@@ -18,6 +18,9 @@ function weather(response) {
 	document.querySelector("h2").innerHTML = response.data.name;
 	//returns the location name
 
+	document.querySelector(".theweather").innerHTML =
+		response.data.weather[0].main;
+
 	document.querySelector(".tempdigits").innerHTML = Math.round(
 		response.data.main.temp
 	);
@@ -32,21 +35,52 @@ function weather(response) {
 		".windSpeed"
 	).innerHTML = `Wind Speed: ${velocityofwind}`;
 	//the above provides the wind speed
-
-	console.log(response);
 }
 
-function locationOne(position) {
+function searchlocation(position) {
 	let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
 	let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
 	axios.get(apiUrl).then(weather);
 }
 
-function currentlocation() {
-	let locationText = document.querySelector("h2");
-	locationText.innerHTML =
-		navigator.geolocation.getCurrentPosition(locationOne);
+function currentlocation(event) {
+	event.preventDefault();
+	navigator.geolocation.getCurrentPosition(searchlocation);
+}
+
+function lightblue(city) {
+	let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+	let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+	axios.get(apiUrl).then(weather);
+}
+function darkblue(event) {
+	event.preventDefault();
+	let city = document.querySelector("#search-text-input").value;
+	lightblue(city);
+}
+
+function conversionCtoF(event) {
+	let initial = document.querySelector(".tempdigits").innerHTML;
+	let fahrenheit = initial * (9 / 5) + 32;
+	let fdegrees = Math.round(fahrenheit);
+	document.querySelector(".tempdigits").innerHTML = fdegrees;
+}
+
+function conversionFtoC(event) {
+	let initialvalue = document.querySelector(".tempdigits").innerHTML;
+	let celsius = (5 / 9) * (initialvalue - 32);
+	let cdegrees = Math.round(celsius);
+	document.querySelector(".tempdigits").innerHTML = cdegrees;
 }
 
 let thebutton = document.querySelector("button");
 thebutton.addEventListener("click", currentlocation);
+
+let bluebutton = document.querySelector("#search-form");
+bluebutton.addEventListener("submit", darkblue);
+
+let cell = document.querySelector(".ren-to-cel");
+cell.addEventListener("click", conversionFtoC);
+
+let ren = document.querySelector(".cel-to-ren");
+ren.addEventListener("click", conversionCtoF);
