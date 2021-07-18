7202f70aa2a0e52bd121bbef349831971a503aaa
SMei201900 <smei201900@gmail.com>
2021-07-17 21:49:05 -0400

    Updates

--- script.js
+++ script.js
@@ -1,31 +1,52 @@
 let now = new Date();
 let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday",
 ];
-let dateandtime = document.querySelector("h3");
+let dateandtime = document.querySelector(".time-and-date");
 dateandtime.innerHTML = `${
-  days[now.getDay()]
+	days[now.getDay()]
 } ${now.getHours()}:${now.getMinutes()}`;
 //returns the current day of the week and time
 
-/*
-    function locationone(position) {
-        let apiKey = "";
-        let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
-        axios.get(apiUrl).then(//);
-    }
+function weather(response) {
+	document.querySelector("h2").innerHTML = response.data.name;
+	//returns the location name
 
-    function currentlocation(event) {
-        event.preventDefault();
-        navigator.geolocation.getCurrentPosition(locationone);
-    }
+	document.querySelector(".tempdigits").innerHTML = Math.round(
+		response.data.main.temp
+	);
+	//the above provides the actual temperature number WITHOUT taking away the C and F
 
-    let thebutton = document.querySelector("button");
-    thebutton.addEventListener("click", currentlocation);
-*/
+	let howhumid = response.data.main.humidity;
+	document.querySelector(".theHumidity").innerHTML = `Humidity: ${howhumid}`;
+	//the above provides the humidity value
+
+	let velocityofwind = response.data.wind.speed;
+	document.querySelector(
+		".windSpeed"
+	).innerHTML = `Wind Speed: ${velocityofwind}`;
+	//the above provides the wind speed
+
+	console.log(response);
+}
+
+function locationOne(position) {
+	let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+	let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+	axios.get(apiUrl).then(weather);
+}
+
+function currentlocation() {
+	let locationText = document.querySelector("h2");
+	locationText.innerHTML =
+		navigator.geolocation.getCurrentPosition(locationOne);
+}
+
+let thebutton = document.querySelector("button");
+thebutton.addEventListener("click", currentlocation);
